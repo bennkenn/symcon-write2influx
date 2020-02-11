fec007f42bb6ee6af039e6219f1529abcdfe4d74
BENJAMIN KENNER <audius.kenner@extaccount.com>
2020-02-11 14:02:23 +0100

    cm

--- influxwriter/module.php
+++ influxwriter/module.php
@@ -1,6 +1,20 @@
 <?php
     // Klassendefinition
     class influxwriter extends IPSModule {
+
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+        }
+
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
@@ -8,7 +22,7 @@
         * ABC_MeineErsteEigeneFunktion($id);
         *
         */
-        public function influxwriter() {
-            echo $this->InstanceID;
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
         }
     }
